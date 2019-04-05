d334d7b657e32be4b1b355618da16e11226b1ccc
Micha Pietsch <michapietsch@mail.de>
2019-04-05 15:55:15 +0200

    Use a closure in PassengerFactory to enable overriding flight_id

--- database/factories/PassengerFactory.php
+++ database/factories/PassengerFactory.php
@@ -5,6 +5,8 @@ use Faker\Generator as Faker;
 $factory->define(App\Passenger::class, function (Faker $faker) {
     return [
         'name' => $faker->name(),
-        'flight_id' => factory(App\Flight::class)->create()->id,
+        'flight_id' => function () {
+            return factory(App\Flight::class)->create()->id;
+        },
     ];
 });
